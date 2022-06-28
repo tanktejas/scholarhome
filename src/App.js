6eabcc1ea44eea950ebe70d69e79afc97fd1b50a
Dharmesh177 <dvala453@gmail.com>
2022-06-28 15:54:18 +0530

    final changes in team

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Cards from "./Components/Cards";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
-import Home from "./Components/home/home";
+import Home from "./Components/home/hom";
 import Header from "./Components/Navbar/Header"
 import AboutUsPage from "./Components/AboutUsPage";
 import AllScho from "./Components/home/AllScho";
-import Team from "./Components/Team/Team"
+import Team from "./Components/Team/Tea"
 
 function App() {
   return (
